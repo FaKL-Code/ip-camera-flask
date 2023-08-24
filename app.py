696f54249e9103c284d4fa8b83a52c1e488fcb02
Julio Facal <starlyn.dev@gmail.com>
2023-08-24 17:49:40 -0300

    basic app

--- app.py
+++ app.py
@@ -4,22 +4,10 @@ import cv2
 app = Flask(__name__)
 
 camera = cv2.VideoCapture(0)
-camera2 = cv2.VideoCapture(1)
 
-def gen_frames1():  
+def gen_frames():  
     while True:
         success, frame = camera.read()  
-        if not success:
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  
-
-def gen_frames2():  
-    while True:
-        success, frame = camera2.read()  
         if not success:
             break
         else:
@@ -33,13 +21,9 @@ def gen_frames2():
 def index():
     return render_template('index.html')
 
-@app.route('/video_feed_1')
-def video_feed_1():
-    return Response(gen_frames1(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-@app.route('/video_feed_2')
-def video_feed_2():
-    return Response(gen_frames2(), mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
     app.run(debug=True)
